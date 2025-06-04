be1f16202356e7f849c562e50437f3d526d08d06
Yahya ElAraby <yaya1426@gmail.com>
2025-06-04 21:38:40 +0300

    Add deploy to VPS action

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello From Hostinger!';
+    return 'Hello From Hostinger VPS!';
   }
 }
