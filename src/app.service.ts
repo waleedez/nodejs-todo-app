313e34a2abc1cef39ba777de748fe44c48b49bbf
Waleed EzzElden <waleed.ez_elden@syngenta.com>
2025-06-13 22:54:55 +0300

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello From OCI VPS!';
+    return 'Hello From OCI VPS! :)';
   }
 }
